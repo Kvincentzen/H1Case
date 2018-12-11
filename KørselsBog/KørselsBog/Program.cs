70967437aa47cddc6029c555cbd2d1a63c485fe7
Kristian Vincentzen <kris458a@elev.tec.dk>
2018-12-11 09:44:20 +0100

    Ya

--- KørselsBog/KørselsBog/Program.cs
+++ KørselsBog/KørselsBog/Program.cs
@@ -10,7 +10,7 @@ namespace KørselsBog
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello Penis");
         }
     }
 }
