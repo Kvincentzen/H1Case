121519d66796c5ea7450d1a2945967e89b9bec83
Kristian Vincentzen <kris458a@elev.tec.dk>
2018-12-11 10:21:09 +0100

    big one

--- KørselsBog/KørselsBog/Sql-Methods.cs
+++ KørselsBog/KørselsBog/Sql-Methods.cs
@@ -8,6 +8,50 @@ namespace KørselsBog
 {
     class Sql_Methods
     {
+        static void Main(string[] args)
+        {
+            //Opretter en kunde
+            //opretKunde();
+            SqlTest.Select("select*from kunder");
+            Console.ReadKey();
 
-    }
-}
+        }
+        private static void opretKunde()
+        {
+            string statement = "insert into kunder values ('Knud Andersen','Telegrafvej 9', 45)";
+            //string statement = "insert into kunder values ('Knud Andersen','Telegrafvej 9', 45)";
+            SqlTest.Insert(statement);
+        }
+        //ConnectionString
+        private static string ConnectionString = "Data Source=Skab5-PC-01;Initial Catalog=sqleksempler; Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public static void Insert(string sql)
+        {
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public static void Select(string sql)
+        {
+            DataTable table = new DataTable();
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(sql, con);
+                adapter.Fill(table);
+
+                //foreach (DataRow kunde in table.Rows)
+                //{
+                //    Console.WriteLine(kunde["id"].ToString());
+                //    Console.WriteLine(kunde["navn"].ToString());
+                //    Console.WriteLine(kunde["adr"].ToString());
+                //    Console.WriteLine(kunde["alder"].ToString());
+                //    Console.WriteLine();
+                //}
+                string denførsterække = table.Rows[0]["navn"].ToString();
+                Console.WriteLine(denførsterække);
+            }
+        }
