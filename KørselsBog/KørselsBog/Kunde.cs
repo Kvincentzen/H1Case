414b53f29f817fec70f13042254da9d7786a2669
EMIL-O <emilriisottosen@gmail.com>
2019-01-08 08:53:17 +0100

    Biler og kunder (cs) tilføjet

---
+++ KørselsBog/KørselsBog/Kunde.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KørselsBog
+{
+    public class Kunde
+    {
+        public int KundeID { get; set; }
+        public string Fornavn { get; set; }
+        public string Efternavn { get; set; }
+        //public string BilID { get; set; }
+        public DateTime Oprettelsesdato { get; set; }
+    }
+}
